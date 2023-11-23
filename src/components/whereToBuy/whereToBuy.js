449a0d10e8aeebb0a58a2bbe8b1605ce0c198b3f
carolinavc <carjesus92@hotmail.com>
2023-11-23 19:30:30 +0000

    BookDetails post and comments working + Merch details page almost complete

--- src/components/whereToBuy/whereToBuy.js
+++ src/components/whereToBuy/whereToBuy.js
@@ -2,27 +2,37 @@ import React, { useState, useEffect } from "react";
 import "./whereToBuy.scss";
 import SecondaryButton from "../buttons/SecondaryButton/SecondaryButton";
 import { baseImageLink } from "../../util/axiosBooks";
+import { useParams } from "react-router-dom";
 
 export default function WhereToBuy(props) {
+    const { id } = useParams();
     const [isCheapest, setIsCheapest] = useState(false);
+    const [libraryIsNear, setLibraryIsNear] = useState(false);
 
     useEffect(() => {
-        // Certifique-se de que props.allPrices é uma array antes de usar reduce
         if (Array.isArray(props.allPrices) && props.allPrices.length > 0) {
-            // Verificar se o preço atual é o mais barato na lista
             const isCurrentCheapest = props.allPrices.reduce(
                 (cheapest, price) => (price < cheapest ? price : cheapest),
                 props.allPrices[0]
             );
-
             setIsCheapest(props.preco === isCurrentCheapest);
         }
     }, [props.preco, props.allPrices]);
 
+    useEffect(() => {
+        if (props.user && typeof props.localidade === 'string' && typeof props.user.localidade === 'string') {
+            const isSameLocation = props.localidade.toLowerCase() === props.user.localidade.toLowerCase();
+            setLibraryIsNear(isSameLocation);
+        } else {
+            setLibraryIsNear(false);
+        }
+        console.log("userLocalidade", props.user);
+
+    }, [props.localidade, props.user]);
+
     return (
         <div className={`WhereToBuy ${isCheapest ? "cheapest" : ""}`}>
-            <div
-                className={"image"}
+            <div className={"image"}
                 style={{ backgroundImage: `url(${baseImageLink + props.capa})` }}
             />
 
@@ -33,8 +43,9 @@ export default function WhereToBuy(props) {
                 <p>{props.localidade + ", " + props.distrito}</p>
                 <h4>{props.preco}€</h4>
 
-                <SecondaryButton text={"Adicionar ao carrinho"} />
+                {props.userLocalidade && libraryIsNear && <div className="libraryNearIndicator">Mais perto</div>}
 
+                <SecondaryButton text={"Adicionar ao carrinho"} />
             </div>
         </div>
     );
