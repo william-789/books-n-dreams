2136e1a28b4e127fbe82b683565cebd9bc48ed9b
Estudante 13 <estudante13@email.com>
2023-11-29 12:37:21 +0000

    feat: new page ShoppingCart

---
+++ src/components/userItems/UserLine.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './UserLine.scss'
+
+
+export default function UserLine(props) {
+
+    return (
+        <div className={"line-wrapper"}>
+            <hr/>
+        </div>
+    );
+}
