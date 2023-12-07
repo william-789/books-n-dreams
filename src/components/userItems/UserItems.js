2f0d38dbffb24e4a8e0a36184957bf30291b7c5d
William Rodrigues <riberackw@gmail.com>
2023-12-07 15:49:42 +0000

    feat: receive data from props

--- src/components/userItems/UserItems.js
+++ src/components/userItems/UserItems.js
@@ -1,33 +1,34 @@
 import React from "react";
 import './UserItems.scss'
-import Favorite from "../favorite/favorite";
-import UserLine from "./UserLine";
+import { baseImageLink } from "../../util/axiosBooks";
 
 export default function UseItems(props) {
+    const bgImage = {
+        backgroundImage: `url(${baseImageLink+props.foto})`,
+    };
+
 
     return (
         <div className={"userItems-Library"}>
-            <div className={"userItems-LibraryImage"}></div>
+            <div className={"userItems-LibraryImage"} style={bgImage}></div>
 
             <div className={"userItems-LibraryText"}>
                 <div className={"texts"}>
-                    <div className={"name"}>ALGUÉM FALOU SOBRE NÓS</div>
-                    <div className={"author"}>Irene Valejo</div>
-                    <div className={"library"}><strong>Livraria</strong> Centésima Página</div>
+                    <div className={"name"}>{props.nome}</div>
+                    <div className={"author"}>{props.autor}</div>
+                    <div className={"library"}><strong>Livraria:</strong>{props.livraria}</div>
 
                     <div className={"quantity"}>
                         <div className={"quantity-buttons"}>
-                            <button className={"plus"}>+</button>
-                            <label className={"quantity-value"}>1</label>
-                            <button className={"minus"}>-</button>
-                            <button className={"remove"}>Remover</button>
+                            <button className={"plus"} onClick={props.increment}>+</button>
+                            <label className={"quantity-value"}>{props.unidades}</label>
+                            <button className={"minus"} onClick={props.decrement}>-</button>
+                            <button className={"remove"} onClick={props.remove}>Remover</button>
                         </div>
-                        <span className={"price"}>12,00€</span>
+                        <span className={"price"}>{props.unidades*(+props.preco)}€</span>
                     </div>
                 </div>
             </div>
-
-
         </div>
     );
 }
