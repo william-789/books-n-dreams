6aa3a13297da2711cfbe2558b272ae58795a437d
William Rodrigues <riberackw@gmail.com>
2023-12-07 15:52:22 +0000

    refactor: change button order

--- src/components/userStatusButtons/userStatusButton.js
+++ src/components/userStatusButtons/userStatusButton.js
@@ -1,13 +1,10 @@
 import React from "react";
 import './userStatusButton.scss'
-import {Link} from "react-router-dom";
-import {useState} from "react";
 
 export default function UserButtonStatus(props) {
 
     const handleClick = (index) =>{
         props.setActiveButton(index);
-        console.log(props.activeButton)
 
         switch (index){
             case 0 :
@@ -23,11 +20,11 @@ export default function UserButtonStatus(props) {
         <div className={"userButtonStatus"}>
             <button
                 className={props.activeButton === 0 ? 'active' : ''}
-                onClick={()=> handleClick(0)}>Entregue</button>
+                onClick={()=> handleClick(0)}>A decorrer</button>
             <div>
                 <button
                     className={props.activeButton === 1 ? 'active' : ''}
-                    onClick={()=> handleClick(1)}>A decorrer</button>
+                    onClick={()=> handleClick(1)}>Entregue</button>
             </div>
             <button
                 className={props.activeButton === 2 ? 'active' : ''}
