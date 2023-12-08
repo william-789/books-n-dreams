6125e11e95f571615212f36dbae283b15ae6e8c2
MadalenaDuarte <madalena.duarte0507@gmail.com>
2023-12-08 17:24:25 +0000

    fix: add suggestions when there are no results on merch search page

--- src/pages/search/bookSearch/BookSearch.js
+++ src/pages/search/bookSearch/BookSearch.js
@@ -193,14 +193,14 @@ export default function BookSearch(props) {
                         </div>
                         : <div></div>}
 
-                    {suggestionAuthor ?
+                    {suggestionAuthor && suggestionAuthor.length > 0 ?
                         <div className={"suggestions"}>
                             <p>Porque procurou por {authorList.find((a) => a.id === author)?.nome}</p>
                             <WrapList list={suggestionAuthor}/>
                         </div>
                         : <div></div>}
 
-                    {suggestionBookstore ?
+                    {suggestionBookstore && suggestionBookstore.length > 0 ?
                         <div className={"suggestions"}>
                             <p>Porque procurou por {bookstoreList.find((b) => b.id === bookstore)?.nome}</p>
                             <WrapList list={suggestionBookstore}/>
