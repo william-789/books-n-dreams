03cd35d8627d14426d438c9db67beeb669211469
William Rodrigues <riberackw@gmail.com>
2023-12-11 01:41:06 +0000

    refactor: remove console log

--- src/components/forms/login/LoginForm.js
+++ src/components/forms/login/LoginForm.js
@@ -20,39 +20,38 @@ function LoginForm() {
         closeModal()
       })
       .catch(e => {
-          console.log(e, e.response)
         if(e.response.status === 400) setError('E-mail ou palavra-passe incorretos')
         else setError('Um erro ocorreu. Tente novamente mais tarde.')
       })
   };
 
   return (
-      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-        {error && <div className="error">{error}</div>}
-        <Input
-          label={"E-mail"}
-          type={"email"}
-          name={"email"}
-          control={control}
-          placeholder={"Escreve aqui o teu e-mail"}
-          rules={{ required: "E-mail é obrigatório", pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "E-mail inválido" } }}
-        />
-        {errors.email && <div className="error">{errors.email.message}</div>}
-        <Input
-          label={"Palavra-passe"}
-          type={"text"}
-          name={"password"}
-          control={control}
-          placeholder={"Escreve aqui a tua palavra-passe"}
-          rules={{ required: "Palavra-passe é obrigatória" }}
-        />
-        {errors.password && <div className="error">{errors.password.message}</div>}
-        <div className={"button-container"}>
-          <button className={"submit"} type={"submit"}>
-            Submeter
-          </button>
-        </div>
-      </form>
+    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      {error && <div className="error">{error}</div>}
+      <Input
+        label={"E-mail"}
+        type={"email"}
+        name={"email"}
+        control={control}
+        placeholder={"Escreve aqui o teu e-mail"}
+        rules={{ required: "E-mail é obrigatório", pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "E-mail inválido" } }}
+      />
+      {errors.email && <div className="error">{errors.email.message}</div>}
+      <Input
+        label={"Palavra-passe"}
+        type={"password"}
+        name={"password"}
+        control={control}
+        placeholder={"Escreve aqui a tua palavra-passe"}
+        rules={{ required: "Palavra-passe é obrigatória" }}
+      />
+      {errors.password && <div className="error">{errors.password.message}</div>}
+      <div className={"button-container"}>
+        <button className={"submit"} type={"submit"}>
+          Submeter
+        </button>
+      </div>
+    </form>
   );
 }
 
